c51417100962b252b8fe3a0c7424d83163b7628e
Aniket Puranik <ani34ket@gmail.com>
2016-07-18 13:57:02 -0700

    ex3 code script

---
+++ day1/ex3.py
@@ -0,0 +1,12 @@
+a="name1"
+b="name2"
+c="name3"
+
+d=raw_input("enter name4: ")
+
+print "{:>30}".format(a)
+print "{:>30}".format(b)
+print "{:>30}".format(c)
+print "{:>30}".format(d)
+
+#print "{>:>30} {>:>30} {>:>30} {>:>30}".format("a" "b" "c" "d")
