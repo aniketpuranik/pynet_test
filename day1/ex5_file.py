932fbbbd86a114761e2ea08c2282ed2709bb748b
Aniket Puranik <ani34ket@gmail.com>
2016-07-18 14:52:46 -0700

    file example 5

---
+++ day1/ex5_file.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python
+
+with open("test_file") as f:
+    for line in f:
+         print line.strip()
+
+f = open ("new_test_file.txt", "w")
+f.write("test\n")
+f.close()
+
+f = open ("new_test_file.txt", "a")
+f.write("appended\n")
+f.close
+
+f = open ("new_test_file.txt")
+for line in f:
+    print line.strip()
+f.close
